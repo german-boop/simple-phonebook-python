8bf8f17f5da001a166e71a1e784b4a64c2ad6158
premier848 <germantranslator848@gmail.com>
2025-12-24 16:31:40 +0330

    Update test_phonebook.py

--- tests/test_phonebook.py
+++ tests/test_phonebook.py
@@ -1,1 +1,13 @@
+from phonebook. core import add_contact, get_contacts, search_contact
 
+def test_add_and_get():
+    add_contact("Ali", "12345")
+    add_contact("Sara", "67890")
+    contacts = get_contacts()
+    assert len(contacts) == 2
+
+def test_search():
+    add_contact("John", "11111")
+    results = search_contact("John")
+    assert len(results) == 1
+    assert results[0]["phone"] == "11111"

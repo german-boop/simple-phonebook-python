7729a5629337d7209b2d426daa54806dd40a2eec
premier848 <germantranslator848@gmail.com>
2025-12-24 16:26:59 +0330

    Update core.py

--- src/phonebook/core.py
+++ src/phonebook/core.py
@@ -1,1 +1,13 @@
+contacts = []
 
+def add_contact(name: str, phone: str):
+    contacts. append({"name": name, "phone": phone})
+
+def get_contacts():
+    return contacts
+
+def search_contact(search_name: str):
+    return [
+        contact for contact in contacts
+        if search_name.lower() in contact['name'].lower()
+    ]
